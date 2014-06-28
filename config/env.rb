ecccba7ffd378da1b960c974a296a66412f87b9e
Francesco Canessa <makevoid@gmail.com>
2014-06-28 06:34:50 +0200

    update to reflect current app standards

--- config/env.rb
+++ config/env.rb
@@ -3,23 +3,26 @@ require 'bundler/setup'
 
 env = ENV["RACK_ENV"] || "development"
 
+
+Bundler.require :default
+
 # require 'dm-core'
 # require 'dm-sqlite-adapter'
 # require 'dm-mysql-adapter'
 # require 'dm-migrations'
 # require 'dm-validations'
-# 
-# user = "" 
-# 
+#
+# user = ""
+#
 # if env == "production"
 #   pass = File.read(File.expand_path "~/.password").strip
-#   user = "root:#{pass}@" 
+#   user = "root:#{pass}@"
 # end
-# 
+#
 # DataMapper.setup :default, "mysql://#{user}localhost/handsonxp_#{env}"
 # DataMapper.setup :default, "sqlite://#{APP_PATH}/db/app.sqlite"
-# 
-# 
+#
+#
 # Dir.glob("#{APP_PATH}/models/*").each do |model|
 #   require model
 # end
--- fcanessa.rb
+++ fcanessa.rb
@@ -1,25 +1,16 @@
-require 'haml'
-require 'sass'
-require 'sinatra/base'
-require 'sinatra/reloader'
-
 path = File.expand_path "../", __FILE__
 APP_PATH = path
 
+require "#{APP_PATH}/config/env"
+
 class Fcanessa < Sinatra::Base
-  require "#{APP_PATH}/config/env"
 
   configure :development do # use thin start
-    register Sinatra::Reloader
-    also_reload ["controllers/*.rb", "models/*.rb"]
     set :public, "public"
     set :static, true
   end
 
-  set :haml, { :format => :html5 }
-  require 'rack-flash'
   enable :sessions
-  use Rack::Flash
   set :method_override, true
 
   def not_found(object=nil)
